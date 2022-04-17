1b9b72ae65c7fa1d765a77093ea4117f772fec0e
HanTJ <41634614+HanTJ@users.noreply.github.com>
2022-04-17 22:22:56 +0900

    led에 표기하는걸로 변경하고 방향전환 추가

--- main.ts
+++ main.ts
@@ -1,25 +1,55 @@
-LCD1IN8.LCD_Init()
-LCD1IN8.LCD_ClearBuf()
-let ledX = 0
-let ledY = 0
-let lcdX = 1
-let lcdY = 1
-basic.forever(function () {
-    led.toggle(ledX, ledY)
-    ledX += 1
+input.onButtonPressed(Button.A, function () {
+    if( direction == 0 ) direction = 3
+    else direction-=1
+})
+input.onButtonPressed(Button.B, function () {
+    if( direction == 4 ) direction = 0
+    else direction+=1
+})
+
+function draw () {
+    led.unplot(ledX, ledY)
+    switch(dirList[direction]){
+        case "R":
+        ledX+=1
+        break
+        case "L":
+        ledX-=1
+        break
+        case "U":
+        ledY-=1
+        break
+        case "D":
+        ledY+=1
+    }
+valueValidate()
+    led.plot(ledX, ledY)
+}
+
+
+function valueValidate () {
+    if (ledX < 0) {
+        ledX = 4
+    }
     if (ledX > 4) {
         ledX = 0
     }
-    LCD1IN8.DrawPoint(
-    lcdX,
-    lcdY,
-    0,
-    DOT_PIXEL.DOT_PIXEL_1
-    )
-    lcdX += 1
-    LCD1IN8.LCD_Display()
-    basic.pause(100)
-})
+    if (ledY < 0) {
+        ledY = 4
+    }
+    if (ledY > 4) {
+        ledY = 0
+    }
+}
+let dirList = ["R", "D", "L","U"]
+let ledX = 0
+let ledY = 0
+LCD1IN8.LCD_Init()
+LCD1IN8.LCD_ClearBuf()
+let lcdX = 1
+let lcdY = 10
+let direction = 0
 basic.forever(function () {
-	
+    draw()
+    basic.pause(200)
 })
